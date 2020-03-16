efa345cfd24dcb2dc41958c5f6fe68bde2417022
Matteo Simonetti <matt.simonetti@hotmail.it>
2020-03-16 21:31:32 +0100

    Added more infos

--- starskyAddon/GameArchives.h
+++ starskyAddon/GameArchives.h
@@ -7,13 +7,12 @@ struct BTWArchive
 	int unk1;//Always = 4
 	unsigned char unk2[6];//Always = D3 07 0B 04 03 0C
 	unsigned char unk3[2];
-	char* archiveName;//null-terminated string
-	//all zeros until byte 0x98
+	char archiveName [128];//null-terminated string - all zeros until byte 0x98 - I assume it's a 128 byte string
 	unsigned int sizeOfSomething;//The value stored here is almost the size of the file
 };
 
 /*
 	Notes:
 	- At the address of "sizeOfSomething" apparently begins another chunk of data.
-	- The string "‰PNG" always occupies a multiple of 4 position
+	- The string "‰PNG" always occupies a multiple of 4 position (maybe it's useless)
 */
