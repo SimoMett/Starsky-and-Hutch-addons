fcc691ed928a35bded64a6c59219769ebf066802
Matteo Simonetti <matt.simonetti@hotmail.it>
2020-03-16 21:55:24 +0100

    Added more infos

--- starskyAddon/GameArchives.h
+++ starskyAddon/GameArchives.h
@@ -8,7 +8,13 @@ struct BTWArchive
 	unsigned char unk2[6];//Always = D3 07 0B 04 03 0C
 	unsigned char unk3[2];
 	char archiveName [128];//null-terminated string - all zeros until byte 0x98 - I assume it's a 128 byte string
-	unsigned int sizeOfSomething;//The value stored here is almost the size of the file
+	unsigned int sizeOfSomething;//The value stored here is almost the size of the file (maybe address?)
+
+	int unk4[13];//unknown 52 bytes
+
+	unsigned int namesEntryStart;//At the pointed address start a string with (probably) names of files contained in the archive
+	unsigned int namesEntrySize;//size of the long string
+	char unkPNG[4];//HiResTex.btw and LoResTex.btw have "‰PNG" here. City.btw doesn't
 };
 
 /*
