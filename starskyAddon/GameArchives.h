c024404627186703e21094ef4b182f046df4c994
Matteo Simonetti <matt.simonetti@hotmail.it>
2020-03-17 16:57:44 +0100

    Added few infos on unknown address

--- starskyAddon/GameArchives.h
+++ starskyAddon/GameArchives.h
@@ -8,13 +8,29 @@ struct BTWArchive
 	unsigned char unk2[6];//Always = D3 07 0B 04 03 0C
 	unsigned char unk3[2];
 	char archiveName [128];//null-terminated string - all zeros until byte 0x98 - I assume it's a 128 byte string
-	unsigned int sizeOfSomething;//The value stored here is almost the size of the file (maybe address?)
+	
+	unsigned int unk6DataChunkStart;
+	unsigned int unk6DataChunkSize;
+	
+	unsigned int unk7;//it's a low number (count of smth?)
+	unsigned int unk8;//in all .btw files is stored the same address as in unk6DataChunkStart
+	unsigned int unk9;//in all .btw files = 0
+	unsigned int unk10;
 
-	int unk4[13];//unknown 52 bytes
+	unsigned int unk11DataChunkStart;
+	unsigned int unk11DataChunkSize;
+	unsigned int unk12DataChunkStart;
+	unsigned int unk12DataChunkSize;
+
+	unsigned int usedFileTypesStringStart;
+	unsigned int usedFileTypesStringSize;
+
+	unsigned int unk5DataChunkStart;
+	unsigned int unk5DataChunkSize;
 
 	unsigned int namesEntryStart;//At the pointed address start a string with (probably) names of files contained in the archive
 	unsigned int namesEntrySize;//size of the long string
-	//char unkPNG[4];//HiResTex.btw and LoResTex.btw have "‰PNG" here. City.btw doesn't -> THAT'S A PNG FILE!!
+	//first file start
 };
 
 /*
